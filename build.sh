6f80030b7d16be756a27cf6fe0fb97a965baca93
seongcheol777 <seongcheol0517@gmail.com>
2026-03-15 16:00:13 +0900

    Fix: build.sh에 superuser 자동 생성 추가

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# 환경변수가 설정된 경우 관리자 계정 자동 생성
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser --noinput || true
+fi
